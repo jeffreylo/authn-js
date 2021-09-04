875a5d12d540965a94fc14b58b78e4d197619355
Jeffrey Lo <jlo@candy.com>
2021-09-03 21:07:47 -0400

    feat: add cookie domain name
    This patch adds a new CookieSessionStore option, `domain`.

--- src/CookieSessionStore.ts
+++ src/CookieSessionStore.ts
@@ -1,8 +1,9 @@
 import { SessionStore } from "./types";
 
 export interface CookieSessionStoreOptions {
-    path?: string;
-    sameSite?: 'Lax' | 'Strict' | 'None';
+  domain?: string;
+  path?: string;
+  sameSite?: "Lax" | "Strict" | "None";
 }
 
 export default class CookieSessionStore implements SessionStore {
@@ -10,24 +11,31 @@ export default class CookieSessionStore implements SessionStore {
   private readonly secureFlag: string;
   private readonly path: string;
   private readonly sameSite: string;
+  private readonly domain: string;
 
   constructor(cookieName: string, opts: CookieSessionStoreOptions = {}) {
     this.sessionName = cookieName;
-    this.secureFlag = (window.location.protocol === 'https:') ? '; secure' : '';
-    this.path = !!opts.path ? `; path=${opts.path}` : '';
-    this.sameSite = !!opts.sameSite ? `; SameSite=${opts.sameSite}` : '';
-
+    this.secureFlag = window.location.protocol === "https:" ? "; secure" : "";
+    this.path = !!opts.path ? `; path=${opts.path}` : "";
+    this.sameSite = !!opts.sameSite ? `; SameSite=${opts.sameSite}` : "";
+    this.domain = !!opts.domain ? `; domain=${opts.domain}` : "";
   }
 
   read(): string | undefined {
-    return document.cookie.replace(new RegExp(`(?:(?:^|.*;\\\s*)${this.sessionName}\\\s*\\\=\\\s*([^;]*).*$)|^.*$`), "$1");
+    return document.cookie.replace(
+      new RegExp(
+        `(?:(?:^|.*;\\\s*)${this.sessionName}\\\s*\\\=\\\s*([^;]*).*$)|^.*$`
+      ),
+      "$1"
+    );
   }
 
   update(val: string) {
-    document.cookie = `${this.sessionName}=${val}${this.secureFlag}${this.path}${this.sameSite}`;
+    document.cookie = `${this.sessionName}=${val}${this.secureFlag}${this.path}${this.sameSite}${this.domain}`;
   }
 
   delete() {
-    document.cookie = this.sessionName + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie =
+      this.sessionName + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
   }
 }
